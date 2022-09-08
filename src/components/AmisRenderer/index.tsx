d6cde350c5b3dde84472a7a2cd04f13d5b5661e1
leon <liwangp2012@163.com>
2022-09-08 17:47:42 +0800

    删除注释

--- src/components/AmisRenderer/index.tsx
+++ src/components/AmisRenderer/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import styles from './index.module.less';
 import { render as renderAmis } from 'amis';
-// import { ToastComponent, AlertComponent, alert, confirm, toast } from 'amis-ui';
 import MyInput from '../MyInput';
 
 const prefix: string = 'amis-renderer';
 
-// console.log(`title:`,MyInput);
 let c = MyInput;
 
 type AmisRendererProps = {
@@ -19,14 +17,9 @@ export default class AmisRenderer extends React.Component<AmisRendererProps> {
     super(props);
   }
 
-  componentDidMount(): void {
-    // console.log(`schema:`, this.props.schema);
-  }
-
   render(): React.ReactNode {
     return (
       <div className={styles[prefix]}>
-        {/* <MyInput /> */}
         {renderAmis(this.props.schema)}
       </div>
     );
